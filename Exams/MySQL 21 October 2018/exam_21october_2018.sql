fcadca56efe6e73006f775a0d4f4a3012886fcef
HristoNakov13 <admiralbiat@gmail.com>
2020-02-06 13:35:29 +0200

    refactor/addition

--- Exams/MySQL 21 October 2018/exam_21october_2018.sql
+++ Exams/MySQL 21 October 2018/exam_21october_2018.sql
@@ -114,59 +114,169 @@ WHERE (
 
 #------5------
 
-SELECT tc.card_number, tc.job_during_journey
-FROM travel_cards tc
-ORDER BY  tc.card_number;
+SELECT card_number, job_during_journey
+FROM travel_cards
+ORDER BY card_number;
 
 #------6------
 
-SELECT c.id, concat_ws(' ', c.first_name, c.last_name) full_name, c.ucn
-FROM colonists c
-ORDER BY c.first_name, c.last_name, c.id;
+SELECT id, 
+	CONCAT_WS(' ', first_name, last_name) full_name,
+    ucn
+FROM colonists
+ORDER BY first_name, last_name, id;
 
 #------7------
 
-SELECT j.id, j.journey_start, j.journey_end
-FROM journeys j
-WHERE j.purpose = 'Military'
-ORDER BY j.journey_start;
+SELECT id,
+	journey_start,
+    journey_end
+FROM journeys
+WHERE purpose = 'Military'
+ORDER BY journey_start;
 
 #------8------
 
-SELECT c.id, concat_ws(' ', c.first_name, c.last_name) full_name
+SELECT c.id,
+	CONCAT_WS(' ', c.first_name, c.last_name) full_name
 FROM colonists c
-JOIN travel_cards tc
+INNER JOIN travel_cards tc
 ON c.id = tc.colonist_id
 WHERE tc.job_during_journey = 'Pilot'
-ORDER BY id;
+ORDER BY c.id;
 
 #------9------
 
-SELECT count(c.id) count
+SELECT COUNT(c.id)
 FROM colonists c
-JOIN travel_cards tc
+INNER JOIN travel_cards tc
 ON c.id = tc.colonist_id
-JOIN journeys j
+INNER JOIN journeys j
 ON tc.journey_id = j.id
 WHERE j.purpose = 'Technical';
 
 #------10------
 
-SELECT ship.name spaceship_name, port.name spaceport_name
-FROM spaceships ship
-JOIN journeys j on ship.id = j.spaceship_id
-JOIN spaceports port ON j.destination_spaceport_id = port.id
-ORDER BY ship.light_speed_rate DESC LIMIT 1;
+SELECT s.name, sp.name
+FROM spaceships s
+INNER JOIN journeys j
+ON s.id = j.spaceship_id
+INNER JOIN spaceports sp
+ON j.destination_spaceport_id = sp.id
+ORDER BY s.light_speed_rate DESC
+LIMIT 1;
 
 #------11------
 
 SELECT s.name, s.manufacturer
 FROM colonists c
-JOIN travel_cards tc
-ON tc.colonist_id = c.id
-JOIN journeys j
-on tc.journey_id = j.id
-JOIN spaceships s
-on j.spaceship_id = s.id
-WHERE year(c.birth_date) > year(DATE_SUB('2019-01-01', INTERVAL 30 YEAR)) and tc.job_during_journey = 'Pilot'
+INNER JOIN travel_cards tc
+ON c.id = tc.colonist_id
+INNER JOIN journeys j
+ON tc.journey_id = j.id
+INNER JOIN spaceships s
+ON j.spaceship_id = s.id
+WHERE tc.job_during_journey = 'Pilot' AND c.birth_date >= '1989-01-01'
 ORDER BY s.name;
+
+#------12------
+
+SELECT p.name planet_name,
+	s.name spaceport_name
+FROM journeys j
+INNER JOIN spaceports s
+ON j.destination_spaceport_id = s.id
+INNER JOIN planets p
+ON s.planet_id = p.id
+WHERE j.purpose = 'Educational'
+ORDER BY s.name DESC;
+
+#------13------
+
+SELECT p.name, COUNT(j.id) journeys_count
+FROM journeys j
+INNER JOIN spaceports s
+ON j.destination_spaceport_id = s.id
+INNER JOIN planets p
+ON s.planet_id = p.id
+GROUP BY p.name
+ORDER BY journeys_count DESC, p.name;
+
+#------14------
+
+SELECT j.id,
+	p.name planet_name,
+    s.name spaceport_name,
+    j.purpose journey_purpose
+FROM journeys j
+INNER JOIN spaceports s
+ON j.destination_spaceport_id = s.id
+INNER JOIN planets p
+ON s.planet_id = p.id
+ORDER BY (j.journey_start - j.journey_end) DESC
+LIMIT 1;
+
+#------15------
+
+SELECT tc.job_during_journey
+FROM journeys j
+INNER JOIN travel_cards tc
+ON j.id = tc.journey_id
+ORDER BY (j.journey_start - j.journey_end), 
+	(SELECT COUNT(tci.job_during_journey) 
+    FROM travel_cards tci 
+    WHERE tci.journey_id = tc.journey_id) 
+LIMIT 1;
+
+#------16------
+
+DELIMITER //
+
+CREATE FUNCTION udf_count_colonists_by_destination_planet (planet_name VARCHAR(30))
+RETURNS INT
+DETERMINISTIC
+BEGIN
+	DECLARE colonists_count INT;
+    SET colonists_count := (SELECT COUNT(tc.colonist_id)
+		FROM planets p
+        INNER JOIN spaceports s
+        ON p.id = s.planet_id
+        INNER JOIN journeys j
+        ON s.id = j.destination_spaceport_id
+        INNER JOIN travel_cards tc
+        ON j.id = tc.journey_id
+        WHERE p.name = planet_name
+        );
+	RETURN colonists_count;
+END //
+
+#SELECT p.name, udf_count_colonists_by_destination_planet('Otroyphus') AS count
+#FROM planets AS p
+#WHERE p.name = 'Otroyphus';
+
+#------17------
+
+DELIMITER //
+
+CREATE PROCEDURE udp_modify_spaceship_light_speed_rate(spaceship_name VARCHAR(50), 
+	light_speed_rate_increse INT(11))
+BEGIN 
+	START TRANSACTION;
+    
+	UPDATE spaceships s
+    SET s.light_speed_rate = light_speed_rate + light_speed_rate_increse
+    WHERE s.name = spaceship_name;
+    
+    IF((SELECT COUNT(*) FROM spaceships sd WHERE sd.name = spaceship_name) = 1)
+		THEN COMMIT;
+        ELSE 
+			SIGNAL SQLSTATE '45000'
+			SET MESSAGE_TEXT = 'Spaceship you are trying to modify does not exists.';
+			ROLLBACK;
+	END IF;
+END //
+
+DELIMITER ;
+
+CALL udp_modify_spaceship_light_speed_rate ('USS d', 55);
+SELECT name, light_speed_rate FROM spaceships WHERE name = 'USS Templar';
